75c5f54057e230cdcb782c989d3e1143aca52415
Khin Baptista <khin.baptista@gmail.com>
2017-09-10 18:46:08 -0300

    Added vulkan instance and debug callback
    TODO: find which extensions to enable (platform-specific)
    + everything else

--- drivers/vulkan/rasterizer_vulkan.h
+++ drivers/vulkan/rasterizer_vulkan.h
@@ -2,6 +2,8 @@
 #ifndef RASTERIZERVK_H
 #define RASTERIZERVK_H
 
+#include <vector>
+
 #include "vulkan/vulkan.hpp"
 
 #include "rasterizer_canvas_vulkan.h"
@@ -20,19 +22,43 @@ class RasterizerVK : public Rasterizer {
 	uint64_t prev_ticks;
 	double time_total;
 
+	/* VULKAN STUFF */
+
+#ifdef NDEBUG
+	const bool enable_validation_layers = false;
+#else
+	const bool enable_validation_layers = true;
+#endif
+
+	vk::Instance vk_instance;
+	vk::DebugReportCallbackEXT vk_debug_callback;
+
+	const std::vector<const char *> vk_instance_layers = {
+		"VK_LAYER_LUNARG_standard_validation"
+	};
+
+	std::vector<const char *> vk_instance_extensions = {};
+
+	void SetupDebugCallback();
+
 public:
 	virtual RasterizerStorage *get_storage();
 	virtual RasterizerCanvas *get_canvas();
 	virtual RasterizerScene *get_scene();
 
-	virtual void set_boot_image(const Ref<Image> &p_image, const Color &p_color, bool p_scale);
+	virtual void set_boot_image(
+			const Ref<Image> &p_image,
+			const Color &p_color,
+			bool p_scale);
 
 	virtual void initialize();
 	virtual void begin_frame();
 	virtual void set_current_render_target(RID p_render_target);
 	virtual void restore_render_target();
 	virtual void clear_render_target(const Color &p_color);
-	virtual void blit_render_target_to_screen(RID p_render_target, const Rect2 &p_screen_rect, int p_screen = 0);
+	virtual void blit_render_target_to_screen(RID p_render_target,
+			const Rect2 &p_screen_rect,
+			int p_screen = 0);
 	virtual void end_frame();
 	virtual void finalize();
 
